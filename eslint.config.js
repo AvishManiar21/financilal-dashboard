8c4e1a6b2263109731f39102883e273e452bd188
AvishManiar21 <avishmaniar24@gmail.com>
2025-09-12 22:24:04 -0400

    fix: update ESLint config to flat format for v9

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,36 @@
-// Simple ESLint config for CommonJS project
-module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'script'
-  },
-  globals: {
-    Chart: 'readonly',
-    fetch: 'readonly'
-  },
-  rules: {
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+// ESLint flat config for CommonJS project
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'script',
+      globals: {
+        // Node.js globals
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        console: 'readonly',
+        setTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearTimeout: 'readonly',
+        clearInterval: 'readonly',
+        Buffer: 'readonly',
+        global: 'readonly',
+        // Browser globals
+        window: 'readonly',
+        document: 'readonly',
+        fetch: 'readonly',
+        Chart: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+    }
   }
-};
+];
